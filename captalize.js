fcfe61caf6c505944b2bb9a4640d5f7f37ec0597
Derrick Hodges <hodgesd@gmail.com>
2022-11-09 09:30:41 +0100

    refactor: :art: function and comments

--- captalize.js
+++ captalize.js
@@ -1,8 +1,21 @@
 
-const sampleText = "hElLo, WORld"
-const lower=sampleText.toLowerCase()
-firstInitial=sampleText.slice(0,1)
-restOfText=sampleText.slice(1,)
-console.log(firstInitial.toUpperCase() + restOfText.toLowerCase())
+// text string to capitalize
+const sampleText = "hElLo, WORld!";
+
+// function to capitalize text
+
+function capitalize(sampleText) {
+    // identify and capitalize the first letter of the string
+    firstInitial=sampleText.charAt(0).toUpperCase();
+
+    // slice and lowercase the rest of the string
+    restOfText=sampleText.slice(1).toLowerCase();
+
+    // concatenate and return the two strings
+    return firstInitial+restOfText;
+}
+
+// call the function and print the result
+console.log(capitalize(sampleText));
 
 
