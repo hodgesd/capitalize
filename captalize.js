1ead56526c177838029196e9693bc66742c66b94
Derrick Hodges <hodgesd@gmail.com>
2022-11-09 09:18:50 +0100

    initial commit

---
+++ captalize.js
@@ -0,0 +1,8 @@
+
+const sampleText = "hElLo, WORld"
+const lower=sampleText.toLowerCase()
+firstInitial=sampleText.slice(0,1)
+restOfText=sampleText.slice(1,)
+console.log(firstInitial.toUpperCase() + restOfText.toLowerCase())
+
+
